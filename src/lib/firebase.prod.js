468a7e652ec60ac3b4a59e8883944d2007e9556d
Misha <mishakl10@ukr.net>
2021-02-01 20:46:01 +0200

    SignIn-Page: use firebase.auth from FirebaseContext

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -1,7 +1,6 @@
 import Firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
-import { seedDatabase } from '../seed'
 
 const config = {
   apiKey: "AIzaSyDozA4nf-D1feicQ-Wxmty-UpWRsyQDIpw",
--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,4 +1,5 @@
 import React, { useState, useContext } from 'react'
+import { useHistory } from 'react-router-dom'
 import { FirebaseContext } from '../context/firebase'
 
 import { HeaderContainer } from '../containers/header'
@@ -8,14 +9,28 @@ import { Form } from '../components'
 import * as ROUTES from '../constants/routes'
 
 export default function SignIn() {
+  const history = useHistory()
   const { firebase } = useContext(FirebaseContext)
   const [emailAddress, setEmailAddress] = useState('')
   const [password, setPassword] = useState('')
   const [error, setError] = useState('')
 
   const isInvalid = password === '' || emailAddress === ''
+
   const handleSignIn = (event) => {
     event.preventDefault()
+
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(emailAddress, password)
+      .then(() => {
+        history.push(ROUTES.BROWSE)
+      })
+      .catch(error => {
+        setEmailAddress('')
+        setPassword('')
+        setError(error.message)
+      })
   }
 
   return (

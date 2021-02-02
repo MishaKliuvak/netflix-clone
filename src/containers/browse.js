dea689799382b6127cff390add7ef6e2f7757a71
Misha <mishakl10@ukr.net>
2021-02-02 18:12:12 +0200

    Browse-Page: create profile component

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react'
+import { SelectProfileContainer } from './profile'
+import { FirebaseContext } from '../context/firebase'
 
 export function BrowseContainer({ slides }) {
+  const [profile, setProfile] = useState({})
+  const [loading, setLoading] = useState(true)
+  const { firebase } = useContext(FirebaseContext)
+  const user = firebase.auth().currentUser || {}
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+    }, 3000)
+  }, [])
+
   return (
-    <p>Hello</p>
+   <>
+    <SelectProfileContainer user={user} setProfile={setProfile}/>
+   </>
   )
 }
